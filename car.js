4bf249208f37cd14a4240495cec429248b645db9
Tomasz Tracz <t@tracz.me>
2022-09-08 10:58:35 +0100

    Add collision with other cars

--- car.js
+++ car.js
@@ -1,111 +1,116 @@
 class Car {
-    constructor(x, y, width, height, controlType, maxSpeed = 10) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+  constructor(x, y, width, height, controlType, maxSpeed = 10) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
 
-        this.speed = controlType === "DUMMY" ? maxSpeed / 2 : 0;
-        this.acc = 0.08;
-        this.maxSpeed = maxSpeed;
-        this.friction = 0.008;
-        this.angle = 0;
-        this.turnAngle = 0.02;
-        this.collision = false;
+    this.speed = controlType === "DUMMY" ? maxSpeed / 5 : 0;
+    this.acc = 0.08;
+    this.maxSpeed = maxSpeed;
+    this.friction = 0.008;
+    this.angle = 0;
+    this.turnAngle = 0.02;
+    this.collision = false;
 
-        this.sensor = new Sensor(this);
+    if (controlType === Controls.TYPE.KEYS) this.sensor = new Sensor(this);
 
-        this.controls = new Controls(controlType);
+    this.controls = new Controls(controlType);
+  }
+
+  update(roadBorders, traffic) {
+    if (this.collision) {
+      return;
+    }
+    this.#move();
+    this.polygon = this.#createPolygon();
+    this.collision = this.#assessDamage(roadBorders, traffic);
+    if (this.sensor) this.sensor.update(roadBorders, traffic);
+  }
+
+  #assessDamage(roadBorders, traffic) {
+    const bordersCollision = roadBorders.some((border) => {
+      if (polysIntersect(this.polygon, border)) return true;
+    });
+    const trafficCollision = traffic.some((car) => {
+      if (polysIntersect(this.polygon, car.polygon)) return true;
+    });
+    return bordersCollision || trafficCollision;
+  }
+
+  #createPolygon() {
+    const points = [];
+    const rad = Math.hypot(this.width, this.height) / 2; // distance from centre to one of the corners
+    const alpha = Math.atan2(this.width, this.height); // angle between diagonal and a long side
+
+    // TR POINT
+    points.push({
+      x: this.x - Math.sin(this.angle - alpha) * rad,
+      y: this.y - Math.cos(this.angle - alpha) * rad,
+    });
+
+    // TL POINT
+    points.push({
+      x: this.x - Math.sin(this.angle + alpha) * rad,
+      y: this.y - Math.cos(this.angle + alpha) * rad,
+    });
+
+    // BL POINT
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad,
+    });
+
+    // BR POINT
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad,
+    });
+
+    return points;
+  }
+
+  #move() {
+    if (this.controls.forward) {
+      this.speed +=
+        this.speed < 0 ? this.acc : this.acc * (1 / (1 + this.speed));
+    } else if (this.controls.reverse) {
+      this.speed -= this.speed > 0 ? this.acc : this.acc / 5;
+    } else if (Math.abs(this.speed) < this.friction * 50) this.speed = 0;
+
+    if (this.speed > this.maxSpeed) this.speed = this.maxSpeed;
+    if (this.speed < -this.maxSpeed / 3) this.speed = -this.maxSpeed / 3;
+
+    let frictionMod = 1;
+    if (this.speed != 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+
+      if (this.controls.left) {
+        this.angle += this.turnAngle * flip;
+        frictionMod += this.speed / 5;
+      }
+      if (this.controls.right) {
+        this.angle -= this.turnAngle * flip;
+        frictionMod += this.speed / 5;
+      }
     }
 
-    update(roadBorders) {
-        this.#move();
-        this.polygon = this.#createPolygon();
+    if (this.speed > 0) this.speed -= this.friction * frictionMod;
+    if (this.speed < 0) this.speed += this.friction * frictionMod;
 
-        this.collision = this.#assessDamage(roadBorders);
-        this.sensor.update(roadBorders);
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = this.collision ? "gray" : "orange";
+    ctx.beginPath();
+    ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+    for (let i = 1; i < this.polygon.length; i++) {
+      ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
     }
+    ctx.fill();
 
-    #assessDamage(roadBorders) {
-        return roadBorders.some((border) => {
-            if (polysIntersect(this.polygon, border)) return true;
-        });
-    }
-
-    #createPolygon() {
-        const points = [];
-        const rad = Math.hypot(this.width, this.height) / 2; // distance from centre to one of the corners
-        const alpha = Math.atan2(this.width, this.height); // angle between diagonal and a long side
-
-        // TR POINT
-        points.push({
-            x: this.x - Math.sin(this.angle - alpha) * rad,
-            y: this.y - Math.cos(this.angle - alpha) * rad
-        });
-
-        // TL POINT
-        points.push({
-            x: this.x - Math.sin(this.angle + alpha) * rad,
-            y: this.y - Math.cos(this.angle + alpha) * rad
-        });
-
-        // BL POINT
-        points.push({
-            x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
-            y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad
-        });
-
-        // BR POINT
-        points.push({
-            x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
-            y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad
-        });
-
-        return points;
-    }
-
-    #move() {
-        if (this.controls.forward) {
-            this.speed +=
-                this.speed < 0 ? this.acc : this.acc * (1 / (1 + this.speed));
-        } else if (this.controls.reverse) {
-            this.speed -= this.speed > 0 ? this.acc : this.acc / 5;
-        } else if (Math.abs(this.speed) < this.friction * 50) this.speed = 0;
-
-        if (this.speed > this.maxSpeed) this.speed = this.maxSpeed;
-        if (this.speed < -this.maxSpeed / 3) this.speed = -this.maxSpeed / 3;
-
-        let frictionMod = 1;
-        if (this.speed != 0) {
-            const flip = this.speed > 0 ? 1 : -1;
-
-            if (this.controls.left) {
-                this.angle += this.turnAngle * flip;
-                frictionMod += this.speed / 5;
-            }
-            if (this.controls.right) {
-                this.angle -= this.turnAngle * flip;
-                frictionMod += this.speed / 5;
-            }
-        }
-
-        if (this.speed > 0) this.speed -= this.friction * frictionMod;
-        if (this.speed < 0) this.speed += this.friction * frictionMod;
-
-        this.x -= Math.sin(this.angle) * this.speed;
-        this.y -= Math.cos(this.angle) * this.speed;
-        console.log(`Speed: ${Math.round(this.speed * 20)} MPH`);
-    }
-
-    draw(ctx) {
-        ctx.fillStyle = this.collision ? "gray" : "orange";
-        ctx.beginPath();
-        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
-        for (let i = 1; i < this.polygon.length; i++) {
-            ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
-        }
-        ctx.fill();
-
-        this.sensor.draw(ctx);
-    }
+    if (this.sensor) this.sensor.draw(ctx);
+  }
 }
--- main.js
+++ main.js
@@ -4,24 +4,26 @@ canvas.width = 200;
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
-const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 5)];
+const traffic = [
+  new Car(road.getLaneCenter(1), -100, 30, 50, Controls.TYPE.DUMMY, 5),
+];
 
 const animate = () => {
-    for (let i = 0; i < traffic.length; i++) {
-        traffic[i].update(road.borders);
-    }
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].update(road.borders, []);
+  }
 
-    car.update(road.borders);
+  car.update(road.borders, traffic);
 
-    canvas.height = window.innerHeight;
+  canvas.height = window.innerHeight;
 
-    ctx.save();
-    ctx.translate(0, -car.y + canvas.height * 0.7);
-    road.draw(ctx);
-    for (let i = 0; i < traffic.length; i++) {
-        traffic[i].draw(ctx);
-    }
-    car.draw(ctx);
-    requestAnimationFrame(animate);
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.7);
+  road.draw(ctx);
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].draw(ctx);
+  }
+  car.draw(ctx);
+  requestAnimationFrame(animate);
 };
 animate();
